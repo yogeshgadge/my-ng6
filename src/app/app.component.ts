96f8f3ce87e5ad2154679c88b1b97f0357521ffe
yogeshgadge <yogeshgadge@gmail.com>
2018-06-18 15:11:08 -0400

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, forwardRef} from '@angular/core';
+import {NG_VALUE_ACCESSOR} from '@angular/forms';
+
+
+export const CBP_TOGGLE_SWITCH_CONTROL_VALUE_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  // tslint:disable-next-line:no-use-before-declare
+  useExisting: forwardRef(() => AppComponent),
+  multi: true
+};
+
 
 @Component({
   selector: 'ngx6-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [CBP_TOGGLE_SWITCH_CONTROL_VALUE_ACCESSOR]
 })
 export class AppComponent {
   title = 'ngx6';
